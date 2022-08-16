7b44ea4ad6c62117c3e6438d994b8bfa33e65799
DeveloperDelowar <programmerdaloar@gmail.com>
2022-08-16 02:15:05 -0700

    place order api created & verfy token updated

--- index.js
+++ index.js
@@ -1,103 +1,122 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
 // default route
-app.get('/', (req, res)=>{
-    res.send("The server is running")
-})
+app.get("/", (req, res) => {
+  res.send("The server is running");
+});
 
 // Verify token
 function verifyToken(req, res, next) {
-   const authorization = req.headers.auth;
-   if(! authorization){
-    return res.status(401).send({message : 'Unauthorize access'})
-   }    
+  const authorization = req.headers.auth;
+  const email = req.query.email;
 
-   const token = authorization.split(" ")[1];
-   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorize access" });
+  }
+  // get access token from a sting;
+  const token = authorization.split(" ")[1];
 
-    if(err){
-      return  res.status(403).send({message : 'Forbidden access'});
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    } else {
+      // Check access token email & api requested email;
+      if(decoded.email === email){
+        return next();
+      }
+      else{
+        return send.status(403).send({message : 'Forbidden access'});
+      }
     }
-    else{
-        res.decoded = decoded;
-        next();
-    }
-   }) 
+  });
 }
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otkxf.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
+async function run() {
+  try {
+    await client.connect();
 
-        // Data base collection
-        const productsCollection = client.db('Aruth').collection('products');
-        const usersCollection = client.db('Aruth').collection('users');
+    // Data base collection
+    const productsCollection = client.db("Aruth").collection("products");
+    const usersCollection = client.db("Aruth").collection("users");
+    const OrderCollection = client.db("Aruth").collection("orders");
 
-        
-        /*==========================================
+    /*==========================================
                 Products API 
-        ============================================*/ 
+        ============================================*/
 
-        // get popular products
-        app.get('/popular-products', async(req, res)=>{
-            const allPopularProducts =  productsCollection.find({popular : true});
-            const skip = allPopularProducts.length - 5;
-            const latestProducts = await  allPopularProducts.skip(skip).toArray();
-            const reverse = latestProducts.reverse();
-           res.send(reverse)
-        });
+    // get popular products
+    app.get("/popular-products", async (req, res) => {
+      const allPopularProducts = productsCollection.find({ popular: true });
+      const skip = allPopularProducts.length - 5;
+      const latestProducts = await allPopularProducts.skip(skip).toArray();
+      const reverse = latestProducts.reverse();
+      res.send(reverse);
+    });
 
-        // get product info by id 
-        app.get('/product-details/:id', async(req, res)=>{
-            const {id} = req.params;
-            const product = await productsCollection.findOne({_id : ObjectId(id)})
-            res.send(product)
-        });
+    // get product info by id
+    app.get("/product-details/:id", async (req, res) => {
+      const { id } = req.params;
+      const product = await productsCollection.findOne({ _id: ObjectId(id) });
+      res.send(product);
+    });
 
+    /*==========================================
+                Order management
+        ============================================*/
 
-         /*==========================================
+    // place order
+    app.post("/place-order", verifyToken, async (req, res) => {
+      const ordersInfo = req.body;
+      const result = await OrderCollection.insertOne(ordersInfo);
+      res.send(result);
+      
+    });
+
+    /*==========================================
                 Login & Register
-        ============================================*/ 
+        ============================================*/
 
-        //  send Token after login
-        app.get('/access-token', async(req, res) =>{
-            const {email} = req.query;
-            const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {
-                expiresIn : '1y'
-            });
+    //  send Token after login
+    app.get("/access-token", async (req, res) => {
+      const { email } = req.query;
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1y",
+      });
 
-            res.send({token});
-        });
+      res.send({ token });
+    });
 
-        // Register
-        app.put('/register', async(req, res)=>{
-            const email = req.query.email;
-            const userDoc = {$set : req.body};
-            const query = {email};
-            const option = {upsert : true};
-            const result = await usersCollection.updateOne(query, userDoc, option);
-            res.send(result);
-        });
-    }
-    finally{
-
-    }
+    // Register
+    app.put("/register", async (req, res) => {
+      const email = req.query.email;
+      const userDoc = { $set: req.body };
+      const query = { email };
+      const option = { upsert: true };
+      const result = await usersCollection.updateOne(query, userDoc, option);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-app.listen(PORT, ()=>{
-    console.log('The app is running');
-})
+app.listen(PORT, () => {
+  console.log("The app is running");
+});
